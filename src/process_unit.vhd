e2bc7beda308feea5b4862a79fabe4a6259d72fc
Sanassi Cissé <sanassi-mory.cisse@epita.fr>
2024-05-05 21:43:35 +0200

    feat: started assembling the processing unit

---
+++ src/process_unit.vhd
@@ -0,0 +1,12 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+
+entity process_unit is
+    port (  clk : in std_logic;
+            key : in std_logic_vector(1 downto 0);
+         );
+end entity;
+
+architecture rtl of process_unit is
+begin
+end architecture;
