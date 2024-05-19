a5bbc0bcaf86e92b0aed9df8b1a0f000e5042e3a
Clara Rossi Carignano <clara.rossi-carignano@epita.fr>
2024-05-19 12:41:42 +0200

    feat: some test for data memory but one is failing

--- tests/process_unit_tb.vhd
+++ tests/process_unit_tb.vhd
@@ -6,7 +6,7 @@ entity process_unit_tb is
 end entity process_unit_tb;
 
 architecture testbench of process_unit_tb is
-    constant Period : time := 1 us; -- speed up simulation with a 100kHz clock
+    constant Period : time := 1 us;
 
     signal CLK      : std_logic := '0';
     signal RST      : std_logic := '1';
@@ -52,7 +52,7 @@ begin
         ALU_op <= opVal;
         wait for Period;
         assert (s = exp_value) report test_name & " failed "
-        & "exp : " & integer'image(to_integer(signed(exp_value))) & "  got : "  & 
+        & "exp : " & integer'image(to_integer(signed(exp_value))) & "  got : "  &
         integer'image(to_integer(signed(s))) severity error;
         wait for Period;
         r_regWr <= '1';
@@ -95,7 +95,7 @@ begin
         s_ALUsrc <= '1'; -- ALU src is imm8
         wait for Period;
         assert (s = exp_value) report test_name & " failed "
-        & "exp : " & integer'image(to_integer(signed(exp_value))) & "  got : "  & 
+        & "exp : " & integer'image(to_integer(signed(exp_value))) & "  got : "  &
         integer'image(to_integer(signed(s))) severity error;
         wait for Period;
         r_regWr <= '1';
@@ -114,6 +114,68 @@ begin
 
 end procedure;
 
+procedure memory_write_test(
+                  constant test_name : in string;
+                  constant exp_value : in std_logic_vector(31 downto 0);
+                  constant addr_exp_value : in std_logic_vector(31 downto 0);
+                  constant opVal  : in  std_logic_vector(2 downto 0);
+                  constant dst : in std_logic_vector(3 downto 0);
+                  constant srcN : in  std_logic_vector(3 downto 0);
+                  constant srcM : in  std_logic_vector(3 downto 0);
+                  constant imm8Value : in  std_logic_vector(7 downto 0);
+                  constant ALUsrc_val : in  std_logic;
+                  signal s_clk      : out std_logic;
+                  signal s_rst      : out std_logic;
+                  signal s_ALUsrc      : out std_logic;
+                  signal s_MemWr      : out std_logic;
+                  signal s_Wsrc     : out std_logic;
+                  signal s_imm8 : out  std_logic_vector(7 downto 0);
+                  signal ALU_op  : out  std_logic_vector(2 downto 0);
+                  signal r_rd  :  out std_logic_vector(3 downto 0);
+                  signal r_rn  :  out std_logic_vector(3 downto 0);
+                  signal r_rm  :  out std_logic_vector(3 downto 0)
+) is
+begin
+        -- WRITE REGISTER VALUE IN DATA MEMORY
+        s_rst <= '0';
+        s_clk <= '1';
+        r_rn <= srcN;
+        r_rd <= dst;
+        r_rm <= srcM;
+        ALU_op <= opVal;
+        s_imm8 <= imm8Value;
+        s_ALUsrc <= ALUsrc_val; -- use imm8 or register value
+        s_MemWr <= '1'; -- store in Data Memory register
+        s_Wsrc <= '0';
+        -- the output of the ALU represent the address of data memory register
+        wait for Period;
+        assert (s = addr_exp_value) report test_name &
+        " failed, address value did not match : "
+        & integer'image(to_integer(signed(addr_exp_value)))
+        & " got : "  & integer'image(to_integer(signed(s))) severity error;
+
+        s_clk <= '0';
+        s_MemWr <= '0'; -- only reading
+        s_Wsrc <= '1'; -- W / s is now DataOut of DataMemory entity
+
+        wait for Period;
+        assert (s = exp_value) report test_name & " failed, data mem "
+        & "register value does not match : "
+        & integer'image(to_integer(signed(exp_value)))
+        & " got : "  & integer'image(to_integer(signed(s))) severity error;
+
+        wait for Period;
+        s_clk <= '0';
+        s_rst <= '1';
+        s_ALUsrc <= '0';
+        s_Wsrc <= '0';
+        s_ALUsrc <= '0';
+        wait for Period;
+
+end procedure;
+
+
+
 begin
 
 process
@@ -123,9 +185,9 @@ process
         -- can begin testing
 
         -- R(1) = R(15), copie d'une valeur d'un registre à un autre
-        registers_test("R(1) = R(15)", X"00000030", "011", "0001", "1111", "0000", clk, 
+        registers_test("R(1) = R(15)", X"00000030", "011", "0001", "1111", "0000", clk,
         rst, regWr, ALUctr, rd, rn, rm);
-        -- R(1) = R(1) + R(15), addition de 2 registres 
+        -- R(1) = R(1) + R(15), addition de 2 registres
         registers_test("R(1) = R(1) + R(15)", X"00000030", "000", "0001", "0001", "1111",
         clk, rst, regWr, ALUctr, rd, rn, rm);
         -- R(2) = R(1) + R(15)
@@ -151,8 +213,15 @@ process
         -- R(7) = R(15) - 5
         imm_value_test("R(7) = R(15) - 5", X"0000002b", "010", "0101", "1111",
         "00000101", clk, rst, ALUsrc, imm8, regWr, ALUctr, rd, rn);
-        -- ecriture d'un registre dans un mot de la memoire
-        -- lecture d'un mot de la memoire dans un registre
+        -- ecriture d'un registre dans un mot de la memoire + lecture
+        memory_write_test("Mem[R(2)] = R(15)", X"00000030", X"00000000",
+        "011","0000", "0010", "1111", X"00", '0',
+        clk, rst, ALUsrc, MemWr, Wsrc, imm8, ALUctr, rd, rn, rm );
+
+        memory_write_test("Mem[R(15)] = R(15)", X"00000030", X"00000030",
+        "011","0000", "1111", "1111", X"00", '0',
+        clk, rst, ALUsrc, MemWr, Wsrc, imm8, ALUctr, rd, rn, rm );
+
 
         done <= true;
         wait;
